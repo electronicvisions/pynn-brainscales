3d004f7e8b6deb0b30f9e0c9d307add49603ac84
Eric Müller <mueller@kip.uni-heidelberg.de>
2020-10-05 09:27:09 +0200

    Adjust test condition for current setup condition
    Change-Id: I74acd906ab17130fca6f54c30edd95a9f96d60eb

--- brainscales2/test/hw/test_internal_projections.py
+++ brainscales2/test/hw/test_internal_projections.py
@@ -5,8 +5,8 @@ from examples.internal_projections import main
 class TestTryProjections(unittest.TestCase):
     def test_spikes(self):
         spike_number, _, _ = main()
-        spike_expectation = 20
-        spike_range = 2
+        spike_expectation = 25
+        spike_range = 3
         spike_difference = abs(spike_expectation - spike_number)
         self.assertLessEqual(spike_difference, spike_range)
 
