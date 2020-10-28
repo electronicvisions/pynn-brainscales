bfcbac4c99d58df240af63572a8b978e9700eba7
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2020-10-28 17:59:18 +0100

    Add empty run test
    Change-Id: I4deb87c547e1a51478451db7cd86c9e2e0c96ef0

---
+++ brainscales2/test/hw/test_empty.py
@@ -0,0 +1,14 @@
+import unittest
+import pynn_brainscales.brainscales2 as pynn
+
+
+class TestEmpty(unittest.TestCase):
+    @staticmethod
+    def test_empty():
+        pynn.setup()
+        pynn.run(0.2)
+        pynn.end()
+
+
+if __name__ == "__main__":
+    unittest.main()
