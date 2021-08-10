b8dab26f85f8bba05724b7e25806f048d87fcdab
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2021-08-10 11:16:08 +0200

    Add performance "test" in swtests
    * test does not assert anything but prints performance for manual checking
    
    Change-Id: I28b2856710905afa59cf87d535fcf5349027ab3d

---
+++ brainscales2/test/sw/pynn_brainscales2_test_performance.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import time
+import unittest
+import numpy as np
+import pynn_brainscales.brainscales2 as pynn
+from dlens_vx_v2 import logger
+
+
+logger.set_loglevel(logger.get('TestPerformance'), logger.LogLevel.INFO)
+
+
+# This only executes and prints performance, no assertions are made
+class TestPerformance(unittest.TestCase):
+    def setUp(self):
+        pynn.setup()
+
+    def tearDown(self):
+        pynn.end()
+
+    @classmethod
+    def test_many_small_projections(cls):
+        log = logger.get("TestPerformance.test_many_small_projections")
+        begin = time.time()
+        pops = []
+        for i in range(256):
+            pop = pynn.Population(1, pynn.cells.HXNeuron())
+            # wrap as View because using it in projections is faster
+            pops.append(pynn.PopulationView(pop, np.array(range(len(pop)))))
+
+        projs = []
+        for i in range(256):
+            for j in range(256):
+                projs.append(pynn.Projection(pops[i], pops[j],
+                                             pynn.AllToAllConnector()))
+        log.INFO("network construction {}s".format(time.time() - begin))
+
+        begin = time.time()
+        pynn.run(None)
+        log.INFO("first run {}s".format(time.time() - begin))
+
+        begin = time.time()
+        projs[-1].set(weight=10)
+        log.INFO("network modification (weights) {}s".format(
+            time.time() - begin))
+
+        for i in range(10):
+            begin = time.time()
+            pynn.run(None)
+            log.INFO("run {} after modification {}s".format(
+                i, time.time() - begin))
+
+    @classmethod
+    def test_few_large_projections(cls):
+        log = logger.get("TestPerformance.test_few_large_projections")
+        begin = time.time()
+        pop = pynn.Population(256, pynn.cells.HXNeuron())
+        pops = []
+        # wrap as View because using it in projections is faster
+        pops.append(pynn.PopulationView(pop, np.array(range(len(pop)))))
+
+        projs = []
+        projs.append(pynn.Projection(pops[0], pops[0],
+                                     pynn.AllToAllConnector()))
+        log.INFO("network construction {}s".format(time.time() - begin))
+
+        begin = time.time()
+        pynn.run(None)
+        log.INFO("first run {}s".format(time.time() - begin))
+
+        begin = time.time()
+        projs[-1].set(weight=10)
+        log.INFO("network modification (weights) {}s".format(
+            time.time() - begin))
+
+        for i in range(10):
+            begin = time.time()
+            pynn.run(None)
+            log.INFO(
+                "run {} after modification {}s".format(i, time.time() - begin))
+
+
+if __name__ == '__main__':
+    unittest.main()
