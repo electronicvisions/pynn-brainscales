d144bfb1a2e4fc82f06e8f832fdda7e4df4a8abe
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2021-10-04 10:37:48 +0000

    Testing passing in an existing connection
    Change-Id: I1004a293d9a3b0fe71d9734351cb3233ff307b09

---
+++ brainscales2/test/hw/pynn_brainscales2_test_pass_in_connection.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import unittest
+import pynn_brainscales.brainscales2 as pynn
+
+from dlens_vx_v2 import hxcomm
+
+
+class TestPassInConnection(unittest.TestCase):
+    @staticmethod
+    def test_pass_in_connection():
+        with hxcomm.ManagedConnection() as connection:
+            for _ in range(5):
+                pynn.setup(connection=connection)
+                pynn.Population(512, pynn.cells.HXNeuron())
+                pynn.run(0.2)
+                pynn.end()
+
+
+if __name__ == "__main__":
+    unittest.main()
