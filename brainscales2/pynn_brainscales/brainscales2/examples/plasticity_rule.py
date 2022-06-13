02d482a8a3044ad59d78e5af95d7aaa8b15a57c2
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2022-06-13 10:16:36 +0200

    Adapt to irregularlysampledsignals change in MADC recording return
    Change-Id: Ief1cfe9d136e0fe91f084fc401b0f48bdd04635a

--- brainscales2/pynn_brainscales/brainscales2/examples/plasticity_rule.py
+++ brainscales2/pynn_brainscales/brainscales2/examples/plasticity_rule.py
@@ -85,8 +85,9 @@ def main(params: dict):
     pynn.run(10)
 
     spiketrain = nrn.get_data("spikes").segments[0].spiketrains[0]
-    mem_v = nrn.get_data("v").segments[0]
-    membrane_times, membrane_voltage = zip(*mem_v.filter(name="v")[0])
+    mem_v = nrn.get_data("v").segments[0].irregularlysampledsignals[0]
+    membrane_times = mem_v.times
+    membrane_voltage = mem_v.magnitude
 
     pynn.end()
 
