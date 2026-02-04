993d8e3ebc7c6941c9a07cc53ed2c05809c77dfc
Jakob Kaiser <jakob.kaiser@kip.uni-heidelberg.de>
2026-02-04 15:34:01 +0000

    feat: use CADC in multicompartment example
    Change-Id: I46d6daded37d023627e5bc7855611306af84102a

--- brainscales2/pynn_brainscales/brainscales2/examples/multicompartment.py
+++ brainscales2/pynn_brainscales/brainscales2/examples/multicompartment.py
@@ -8,60 +8,99 @@ from pynn_brainscales.brainscales2.morphology import create_mc_neuron, \
     Compartment, SharedLineConnection
 
 
-def main():
-    '''
-    Create a chain of compartments with three compartments.
+def create_neuron_class(length: int):
+    """
+    Create a neuron class for a dendritic chain.
 
-    Record the membrane voltage in the first compartment while synaptic inputs
-    are injected into it.
-    A plot in which the membrane voltage and recorded spikes are displayed is
-    saved to disk.
+    :param length: Number of compartments in the chain.
+    """
+    #
+    #    ┌─┐ ┌─┐       ┌─┐
+    #    $ │ $ │       $ │
+    #  0─0 1─1 2─...─N-1 N─N
+    compartments = []
+    connections = []
+    for n_comp in range(length):
+        first = n_comp * 2
+        second = first + 1
+
+        connect_shared_line = []
+        if n_comp > 0:
+            connect_shared_line.append(first)
+
+        connect_conductance = []
+        if n_comp < length - 1:
+            # connect to the right
+            connect_conductance.append((second, 200))
+            connections.append(
+                SharedLineConnection(start=second,
+                                     stop=second + 1,
+                                     row=0))
+        compartments.append(
+            Compartment(positions=[first, second],
+                        label=f'comp{n_comp}',
+                        connect_shared_line=connect_shared_line,
+                        connect_conductance=connect_conductance))
+
+    return create_mc_neuron('McNeuron',
+                            compartments=compartments,
+                            connections=connections,
+                            single_active_circuit=True)
+
+
+def main(length: int = 3,
+         runtime: float = 0.15):
     '''
-    compartments = [
-        Compartment(positions=[0], label='my_label',
-                    connect_shared_line=[0]),
-        Compartment(positions=[1, 2], label='my_label',
-                    connect_conductance=[(1, 200)],
-                    connect_shared_line=[2]),
-        Compartment(positions=[3], label='my_label',
-                    connect_conductance=[(3, 200)])]
-    connections = [SharedLineConnection(start=0, stop=1, row=0),
-                   SharedLineConnection(start=2, stop=3, row=0)]
-    McNeuron = create_mc_neuron('McNeuron',
-                                compartments=compartments,
-                                connections=connections)
+    Create a chain of compartments.
+
+    Record the membrane voltage in each compartment while inputs are injected
+    in the different compartments.
+
+    :param length: Length of the chain.
+    :param runtime: Experiment runtime in ms. Note that for runtimes over
+        around 0.15 ms, samples from the CADC are lost, i.e. the data is
+        not recorded of the whole experiment time.
+    '''
+    McNeuron = create_neuron_class(length)
 
     pynn.setup(initial_config=pynn.helper.chip_from_nightly())
 
-    spike_times = np.arange(0.01, 0.2, 0.05)
+    pop = pynn.Population(1, McNeuron(threshold_enable=False))
 
-    pop_input = pynn.Population(5, pynn.cells.SpikeSourceArray,
-                                cellparams={"spike_times": spike_times})
+    # Distribute spikes over the experiment runtime
+    spike_times = np.linspace(0.01, runtime - 0.05, length)
 
-    pop = pynn.Population(1, McNeuron())
-    pop.record(["spikes", "v"])
+    # Inject one spike in each compartment
+    for n_input in range(length):
+        in_pop = pynn.Population(3, pynn.cells.SpikeSourceArray(
+            spike_times=[spike_times[n_input]]))
 
-    synapse = pynn.standardmodels.synapses.StaticSynapse(weight=63)
-    pynn.Projection(pop_input, pop, pynn.AllToAllConnector(),
-                    synapse_type=synapse)
+        synapse = pynn.standardmodels.synapses.StaticSynapse(weight=63)
+        pynn.Projection(
+            in_pop, pop,
+            pynn.AllToAllConnector(location_selector=f'comp{n_input}'),
+            synapse_type=synapse)
 
-    pynn.run(0.2)
+    # record voltage in each compartment
+    pop.record(
+        "v", locations=[f"comp{n_comp}" for n_comp in range(length)],
+        device='cadc')
 
-    spiketrain = pop.get_data("spikes").segments[0].spiketrains[0]
-    mem_v = pop.get_data("v").segments[0].irregularlysampledsignals[0]
-    membrane_times = mem_v.times
-    membrane_voltage = mem_v.magnitude
+    pynn.run(runtime)
+
+    v_mems = pop.get_data("v").segments[0].irregularlysampledsignals
 
     pynn.end()
 
     # Plot data
-    fig, ax = plt.subplots()
-    ax.plot(membrane_times, membrane_voltage)
-    ax.set_xlabel("Time [ms]")
-    ax.set_ylabel("Membrane Potential [LSB]")
+    fig, axs = plt.subplots(length, sharex=True, sharey=True)
 
-    for spike in spiketrain:
-        ax.axvline(spike, c='r')
+    for n_comp, (ax, v_mem) in enumerate(zip(axs, v_mems)):
+        ax.set_title(f'Compartment {n_comp}')
+        ax.plot(v_mem.times, v_mem)
+        ax.axvline(spike_times[n_comp], c='b', ls=':')
+    axs[int(length / 2)].set_ylabel("Membrane Potential [LSB]")
+    axs[-1].set_xlabel("Time [ms]")
 
     fig.savefig("multicompartment_example.png")
 
